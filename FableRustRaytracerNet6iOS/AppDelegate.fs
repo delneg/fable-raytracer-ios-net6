ea08a2494113af2218cd81fe7ce674c44e158e44
Denis Bobrov <delneg@yandex.ru>
2022-11-24 12:03:39 +0400

    Update to .net 7, apple silicon

--- FableRustRaytracerNet6iOS/AppDelegate.fs
+++ FableRustRaytracerNet6iOS/AppDelegate.fs
@@ -12,11 +12,11 @@ module Native =
     
     [<DllImport(DllName, CallingConvention=CallingConvention.Cdecl)>]
     //(value1: i32, value2: i32) -> i32
-    extern int32 add_values(nint value1, nint value2)
+    extern int32 add_values(nativeint value1, nativeint value2)
     
     [<DllImport(DllName, CallingConvention=CallingConvention.Cdecl)>]
     //(x: i32, y: i32, w: i32, h: i32, angle: f64) -> *const u8
-    extern IntPtr render_scene(nint x, nint y, nint w, nint h, float angle)
+    extern IntPtr render_scene(nativeint x, nativeint y, nativeint w, nativeint h, float angle)
     
     let getUIImageForRGBAData width height (dataPtr:IntPtr) (dataLen:int) =
         // https://gist.github.com/irskep/e560be65163efcb04115
@@ -39,14 +39,14 @@ type MyViewController() =
             let xa, ya, wa, ha = ( 0, 0, 128, 128)
             let lenA = wa * ha * 4
             let angleA = (float degrees) * Math.PI / 180.
-            let ret_ptr_A = Native.render_scene(nint xa,nint ya,nint wa,nint ha,angleA)
+            let ret_ptr_A = Native.render_scene(nativeint xa,nativeint ya,nativeint wa,nativeint ha,angleA)
             return Native.getUIImageForRGBAData wa ha ret_ptr_A lenA
         }
     override this.ViewDidLoad() =
         let x, y, w, h = ( 0, 0, 512, 512)
         let angle = 0.0
         let len = w * h * 4        
-        let ret_ptr = Native.render_scene(nint x,nint y,nint w,nint h,angle)
+        let ret_ptr = Native.render_scene(nativeint x,nativeint y,nativeint w,nativeint h,angle)
         let ptr_hex = String.Format("{0:X8}", ret_ptr.ToInt64())
         printfn $"Native big render scene returned 0x{ptr_hex}"
         let imageView = new UIImageView()
@@ -74,7 +74,7 @@ type AppDelegate() =
     override val Window = null with get, set
 
     override this.FinishedLaunching(_, _) =
-        printfn $" Native add_values: 5 + 6 = {Native.add_values(nint 5,nint 6)}"
+        printfn $" Native add_values: 5 + 6 = {Native.add_values(nativeint 5,nativeint 6)}"
         this.Window <- new UIWindow(UIScreen.MainScreen.Bounds)
         let vc = new MyViewController()
         this.Window.RootViewController <- vc
--- FableRustRaytracerNet6iOS/rust-src/build-rust.sh
+++ FableRustRaytracerNet6iOS/rust-src/build-rust.sh
@@ -10,17 +10,20 @@ set -x
 
 # Build binaries
 #cargo +ios-arm64-1.57 build -Zbuild-std --target aarch64-apple-ios --release --lib
-cargo +nightly build -Zbuild-std --target aarch64-apple-ios --release --lib
+#cargo +nightly build -Zbuild-std --target aarch64-apple-ios --release --lib
+cargo +nightly build -Zbuild-std --target aarch64-apple-ios-sim --release --lib
 #cargo  build  --target=x86_64-apple-ios --release
 
 #cargo +ios-arm64-1.57.0 build --target aarch64-apple-ios --release --lib
-cargo build --target=x86_64-apple-ios --release
+#cargo build --target=x86_64-apple-ios --release
 
-strip ./target/x86_64-apple-ios/release/libcore.a
-strip ./target/aarch64-apple-ios/release/libcore.a
+strip ./target/aarch64-apple-ios-sim/release/libcore.a
+#strip ./target/aarch64-apple-ios/release/libcore.a
 
 # Create fat binary
-libtool -static -o ./core/libcore ./target/aarch64-apple-ios/release/libcore.a ./target/x86_64-apple-ios/release/libcore.a
+#libtool -static -o ./core/libcore ./target/aarch64-apple-ios/release/libcore.a ./target/aarch64-apple-ios-sim/release/libcore.a
+
+libtool -static -o ./core/libcore ./target/aarch64-apple-ios-sim/release/libcore.a
 
 # Create C header
 #cbindgen --config cbindgen.toml --crate rust-src --output core/bind_header.h
